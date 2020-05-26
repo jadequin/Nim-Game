2a6bbf9fad01b501f032d434b691fc1519b6707a
Till Nitsche <till.nitsche@mni.thm.de>
2020-05-26 20:50:39 +0200

    better github representation

--- src/Nim.kt
+++ src/Nim.kt
@@ -10,7 +10,9 @@ class Nim(
 
 
     companion object {
-        fun isValidBoardSize(fields: List<Int>) = ceil(log2((fields.max()?:0).toFloat())).toInt() * fields.size < Int.SIZE_BITS
+        fun isValidBoardSize(fields: List<Int>): Boolean {
+            return ceil(log2((fields.max()?:0).toFloat())).toInt() * fields.size < Int.SIZE_BITS
+        }
     }
 
     init {
